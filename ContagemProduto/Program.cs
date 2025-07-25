d2c916d6e3d7cd980f73449b0e988c6aa54c43c9
Samuel Torres <samuelvianatorres@gmail.com>
2025-07-25 19:55:52 -0300

    Adicionar arquivos de projeto.

---
+++ ContagemProduto/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+namespace ContagemProduto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{new String('=', 3)} Calculando o Liquido {new string('=',3)}");
+            Console.WriteLine("----------------------------");
+            Console.Write("Fornecedor: ");
+            string fornecedor = Console.ReadLine().ToUpper();
+            Console.WriteLine("----------------------------");
+            Console.Write("Produto: ");
+            string produto = Console.ReadLine().ToUpper();
+            Console.WriteLine("----------------------------");
+            Console.Write("Peso Bruto: ");
+            double pesoBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("----------------------------");                      
+            Console.Write("Quantidade de Peça: ");
+            int quantidadePeca = int.Parse(Console.ReadLine());
+            Console.WriteLine("----------------------------");
+            Console.Write("Embalagem Peça: ");
+            double embalagemPeca = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("----------------------------");
+            Console.Write("Quantidade de Caixa: ");
+            int quantidadeCaixa = int.Parse(Console.ReadLine());
+            Console.WriteLine("----------------------------");
+            Console.Write("Peso Caixa: ");
+            double pesoCaixa = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("============================");
+            double totalEmbalagem = ((embalagemPeca * quantidadePeca) + pesoCaixa) * quantidadeCaixa;
+            double pesoLiquido = pesoBruto - totalEmbalagem;
+            Console.WriteLine($"Fornecedor: {fornecedor}");
+            Console.WriteLine($"Produto: {produto}");
+            Console.WriteLine($"Peso Bruto: {pesoBruto.ToString("F3", CultureInfo.InvariantCulture)} kg");
+            Console.WriteLine($"Quantidade de Peça: {quantidadePeca}");
+            Console.WriteLine($"Embalagem Peça: {embalagemPeca.ToString("F3", CultureInfo.InvariantCulture)} kg");
+            Console.WriteLine($"Quantidade de Caixa: {quantidadeCaixa}");
+            Console.WriteLine($"Peso Caixa: {pesoCaixa.ToString("F3", CultureInfo.InvariantCulture)} kg");
+            Console.WriteLine($"Total de Embalagem: {totalEmbalagem.ToString("F3", CultureInfo.InvariantCulture)} kg");
+            Console.WriteLine($"Peso Líquido: {pesoLiquido.ToString("F3", CultureInfo.InvariantCulture)} kg");
+            Console.WriteLine("============================");
+
+
+
+
+
+
+
+        }
+    }
+}
