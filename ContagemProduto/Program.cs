186af62c4eec623466def6d57c07c9b4042271d2
Samuel Torres <samuelvianatorres@gmail.com>
2025-07-30 18:56:53 -0300

    Ajuste no cálculo do peso líquido e inclusão do peso do pallet

--- ContagemProduto/Program.cs
+++ ContagemProduto/Program.cs
@@ -8,6 +8,25 @@ namespace ContagemProduto
         {
             Console.WriteLine($"{new String('=', 3)} Calculando o Liquido {new string('=',3)}");
             Console.WriteLine("----------------------------");
+            Console.WriteLine("Informe os dados do produto:");
+            Console.WriteLine("----------------------------");
+            Console.Write("Deseja por o peso do Pallet? S/N ");
+            string resposta = Console.ReadLine().ToUpper();
+            Console.WriteLine("----------------------------");
+            double pesoPallet = 0.0;
+            if (resposta == "S") {                
+                Console.Write("Peso do Pallet: ");
+                pesoPallet = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.WriteLine("----------------------------");
+            }
+            else if (resposta == "N")
+            {
+                pesoPallet = 0.0;
+            }
+            else
+            {
+                Console.WriteLine("Resposta inválida. Considerando peso do pallet como 0.");
+            }
             Console.Write("Fornecedor: ");
             string fornecedor = Console.ReadLine().ToUpper();
             Console.WriteLine("----------------------------");
@@ -32,28 +51,46 @@ namespace ContagemProduto
             Console.Write("Nome da Pessoa: ");
             string nomeUsuario = Console.ReadLine();
             Console.WriteLine("============================");
-            double totalEmbalagem = ((embalagemPeca * quantidadePeca) + pesoCaixa) * quantidadeCaixa;
-            double pesoLiquido = pesoBruto - totalEmbalagem;
-            Console.WriteLine($"Fornecedor: {fornecedor}");
-            Console.WriteLine($"Produto: {produto}");
-            Console.WriteLine($"Peso Bruto: {pesoBruto.ToString("F3", CultureInfo.InvariantCulture)} kg");
-            Console.WriteLine($"Quantidade de Peça: {quantidadePeca}");
-            Console.WriteLine($"Embalagem Peça: {embalagemPeca.ToString("F3", CultureInfo.InvariantCulture)} kg");
-            Console.WriteLine($"Quantidade de Caixa: {quantidadeCaixa}");
-            Console.WriteLine($"Peso Caixa: {pesoCaixa.ToString("F3", CultureInfo.InvariantCulture)} kg");
-            Console.WriteLine($"Total de Embalagem: {totalEmbalagem.ToString("F3", CultureInfo.InvariantCulture)} kg");
-            Console.WriteLine($"Peso Líquido: {pesoLiquido.ToString("F3", CultureInfo.InvariantCulture)} kg");
-            Console.WriteLine("----------------------------");
-            Console.WriteLine($"Data de Pesagem: {DateTime.Now.ToString("dd/MM/yyyy")}");
-            Console.WriteLine($"Hora de Pesagem: {DateTime.Now.ToString("HH:mm:ss")}");
-            Console.WriteLine($"Pesado por: {nomeUsuario}");
-            Console.WriteLine("============================");
+            double totalEmbalagem =((embalagemPeca * quantidadePeca) + pesoCaixa) * quantidadeCaixa ;
+            double pesoLiquido = pesoBruto - totalEmbalagem - pesoPallet;
 
-
-
-
-
-
+            if (pesoPallet != 0.0)
+            {
+                pesoBruto -= pesoPallet;
+                Console.WriteLine($"Fornecedor: {fornecedor}");
+                Console.WriteLine($"Produto: {produto}");
+                Console.WriteLine($"Peso Bruto: {pesoBruto.ToString("F3", CultureInfo.InvariantCulture)} kg");
+                Console.WriteLine($"Quantidade de Peça: {quantidadePeca}");
+                Console.WriteLine($"Embalagem Peça: {embalagemPeca.ToString("F3", CultureInfo.InvariantCulture)} kg");
+                Console.WriteLine($"Quantidade de Caixa: {quantidadeCaixa}");
+                Console.WriteLine($"Peso Caixa: {pesoCaixa.ToString("F3", CultureInfo.InvariantCulture)} kg");
+                Console.WriteLine($"Peso do Pallet: {pesoPallet.ToString("F3", CultureInfo.InvariantCulture)} kg");
+                Console.WriteLine($"Total de Embalagem: {totalEmbalagem.ToString("F3", CultureInfo.InvariantCulture)} kg");
+                Console.WriteLine($"Peso Líquido: {pesoLiquido.ToString("F3", CultureInfo.InvariantCulture)} kg");
+                Console.WriteLine("----------------------------");
+                Console.WriteLine($"Data de Pesagem: {DateTime.Now.ToString("dd/MM/yyyy")}");
+                Console.WriteLine($"Hora de Pesagem: {DateTime.Now.ToString("HH:mm:ss")}");
+                Console.WriteLine($"Pesado por: {nomeUsuario}");
+                Console.WriteLine("============================");
+            }
+            else
+            {
+                Console.WriteLine($"Fornecedor: {fornecedor}");
+                Console.WriteLine($"Produto: {produto}");
+                Console.WriteLine($"Peso Bruto: {pesoBruto.ToString("F3", CultureInfo.InvariantCulture)} kg");
+                Console.WriteLine($"Quantidade de Peça: {quantidadePeca}");
+                Console.WriteLine($"Embalagem Peça: {embalagemPeca.ToString("F3", CultureInfo.InvariantCulture)} kg");
+                Console.WriteLine($"Quantidade de Caixa: {quantidadeCaixa}");
+                Console.WriteLine($"Peso Caixa: {pesoCaixa.ToString("F3", CultureInfo.InvariantCulture)} kg");
+                Console.WriteLine($"Total de Embalagem: {totalEmbalagem.ToString("F3", CultureInfo.InvariantCulture)} kg");
+                Console.WriteLine($"Peso do Pallet: Não foi nescessario!!!");
+                Console.WriteLine($"Peso Líquido: {pesoLiquido.ToString("F3", CultureInfo.InvariantCulture)} kg");
+                Console.WriteLine("----------------------------");
+                Console.WriteLine($"Data de Pesagem: {DateTime.Now.ToString("dd/MM/yyyy")}");
+                Console.WriteLine($"Hora de Pesagem: {DateTime.Now.ToString("HH:mm:ss")}");
+                Console.WriteLine($"Pesado por: {nomeUsuario}");
+                Console.WriteLine("============================");
+            }
 
         }
     }
